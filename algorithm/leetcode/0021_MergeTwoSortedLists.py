8ee490968b88b6352c123df5d4b7cf0b8d808ee0
이원준 <wonjun9090@naver.com>
2020-09-27 23:04:58 +0900

    200927 | LinkedList(Recursion)

---
+++ algorithm/leetcode/0021_MergeTwoSortedLists.py
@@ -0,0 +1,9 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        return l1
+a = Solution()
+print(a.mergeTwoLists([1,2,3], [1,2,3,4]))
