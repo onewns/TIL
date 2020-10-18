c8d488b646145f0d7e0efe4af2db1d51808fd94d
이원준 <wonjun9090@naver.com>
2020-10-18 18:57:33 +0900

    201018 | spa by browser history

--- react_study/src/App.js
+++ react_study/src/App.js
@@ -1,15 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 import TodoList from './TodoList.js'
 
-function App() {
+
+
+export default function App() {
+  const [pageName, setPageName] = useState('');
+  useEffect(() => {
+    window.onpopstate = event => {
+      setPageName(event.state);
+    };
+  }, []);
+  const onClick1 = () => {
+    const pageName = 'page1';
+    window.history.pushState(pageName, '', '/page1');
+    setPageName(pageName);
+  }
+  const onClick2 = () => {
+    const pageName = 'page2';
+    window.history.pushState(pageName, '', '/page2');
+    setPageName(pageName);
+  }
   return (
     <div className="App">
       <TodoList/>
+      <div>
+        <button onClick={onClick1}>page1</button>
+        <button onClick={onClick2}>page2</button>
+        {!pageName && <Home/>}
+        {pageName === 'page1' && <Page1/>}
+        {pageName === 'page2' && <Page2/>}
+      </div>
     </div>
   );
 }
 
-export default App;
+function Home() {
+  return <h2>여기는 홈페이지</h2>
+}
+function Page1() {
+  return <h2>여기는 1</h2>
+}
+function Page2() {
+  return <h2>여기는 2</h2>
+}
