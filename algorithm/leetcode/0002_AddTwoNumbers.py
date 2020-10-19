e8399b8561c2938b6809f44117d9661160a4ee99
이원준 <wonjun9090@naver.com>
2020-10-19 15:55:00 +0900

    201019 | addTwoNumber by ListNode

--- algorithm/leetcode/0002_AddTwoNumbers.py
+++ algorithm/leetcode/0002_AddTwoNumbers.py
@@ -5,5 +5,19 @@ class ListNode:
         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        # my style
-        ans = ListNode()
+        # my style 68ms 14MB
+        ans = head = ListNode(0)
+        flag = 0
+        while l1 or l2 or flag:
+            temp = 0
+            if l1:
+                temp += l1.val
+                l1 = l1.next
+            if l2:
+                temp += l2.val
+                l2 = l2.next
+            flag, v = divmod(temp+flag,10)
+            head.next = ListNode(v)
+            head = head.next
+        return ans.next
+
