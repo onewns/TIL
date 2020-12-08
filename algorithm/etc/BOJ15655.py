7af814bd3476c7c3d9f8051e844d29a012a03dce
wonjun9090 <wonjun9090@naver.com>
2020-12-08 22:24:03 +0900

    2020_12_08 | combination

---
+++ algorithm/etc/BOJ15655.py
@@ -0,0 +1,21 @@
+import sys
+sys.stdin = open('../input.txt', 'r')
+
+
+def combination(depth, length, arr):
+    if len(arr) == length:
+        plist.append(' '.join(arr))
+        return
+    if depth == n:
+        return
+    arr.append(nums[depth])
+    combination(depth + 1, length, arr)
+    arr.pop()
+    combination(depth + 1, length, arr)
+
+
+n, m = map(int, input().split())
+nums = list(map(str, sorted(list(map(int, input().split())))))
+plist = []
+combination(0, m, [])
+print(*plist, sep='\n')
