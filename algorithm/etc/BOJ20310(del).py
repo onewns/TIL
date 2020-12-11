8850c91d5c6f822ab6e90492ad90276e09f78836
wonjun9090 <wonjun9090@naver.com>
2020-12-11 23:23:52 +0900

    2020_12_11 | greedy (del method)

--- algorithm/etc/BOJ20310(del).py
+++ algorithm/etc/BOJ20310(del).py
@@ -1,11 +1,10 @@
 import sys
 sys.stdin = open("../input.txt", 'r')
-# n이 커지면 list를 쓴 것 보다 훨씬 빠름 n^2 인것은 같으나 상수항이 다른듯
-from collections import deque
+# n이 커지면 remove를 쓴 것 보다 훨씬 빠름 n^2 인것은 같으나 상수항이 다른듯
 
 
 def thanos(arr):
-    num = deque(arr)
+    num = arr
     one, zero = num.count(1)//2, num.count(0)//2
     i = 0
     while one:
